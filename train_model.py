8604acfdc474ec4e6be991c3e7e184f11abcd519
rdiehlmartinez <richarddiehl@gmail.com>
2022-01-24 10:10:25 +0000

    set up initial config structure

---
+++ train_model.py
@@ -0,0 +1,57 @@
+__author__ = 'Richard Diehl Martinez'
+
+'''
+Entry point for launching problyglot.
+'''
+
+import logging
+import torch
+import click
+import os
+
+from configparser import ConfigParser
+from src.utils import device, set_seed
+
+'''
+Base utils for setting up training and eval loops.
+'''
+def setup_config(config_file_path):
+    config = ConfigParser()
+    config.read(config_file_path)
+    return config
+
+def setup_logger(config_file_path):
+    # Removing handlers that might be associated with environment; and logs
+    # out to both stderr and a log file
+    experiment_directory = os.path.dirname(os.path.join(os.getcwd(), config_file_path)) 
+
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+    log_file_name = os.path.join(experiment_directory, "experiment.log")
+    logging.basicConfig(
+                        format='%(asctime)s [%(levelname)s] %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p',
+                        level=logging.DEBUG,
+                        handlers=[
+                            logging.FileHandler(log_file_name),
+                            logging.StreamHandler()
+                        ]
+    )
+    logging.info(f"Initializing experiment: {experiment_directory}")
+    logging.info(f"Running model on device: {device}")
+
+def setup(config_file_path):
+    config = setup_config(config_file_path)
+    setup_logger(config_file_path)
+    set_seed(config.getint("EXPERIMENT", "set_seed", fallback=42))
+    return config
+
+# Entry point
+@click.command()
+@click.argument('config_fp')
+def main(config_fp):
+    print("hello world")
+    setup(config_fp)
+
+if __name__ == '__main__':
+    main()
