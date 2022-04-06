19884da61595ba8c8885d7f9428093299763b1f5
rdiehlmartinez <richarddiehl@gmail.com>
2022-04-06 13:33:23 +0100

    moving task head into separate folder

---
+++ src/taskheads/base.py
@@ -0,0 +1,65 @@
+__author__ = 'Richard Diehl Martinez'
+
+from collections import defaultdict
+
+class TaskHead(object):
+    """
+    Everytime we train and evaluate a model using a learner, we need to initialize 
+    and train a 'task head'. For every type of task (e.g. classification, q&a) we can have 
+    different methods for initializing the task head (e.g. randomly). To initialize a task 
+    head, we first define an initialization function that we wrap using the 
+    register_initialization_method() method, and then we can call initialize_task_head() with
+    the appropriate parameters.
+    """
+
+    _task_head_initializers = defaultdict(dict)
+
+    @classmethod
+    def register_initialization_method(cls, initialization_function):
+        """ 
+        Decorator function that takes in an initialization function and registers this 
+        as a function to use for intitializing the weights of a task head.
+        Args: 
+            * initialization_function (type.function): A function that initializes task 
+                head parameters 
+        """
+        task_type, method = initialization_function.__name__.split('_', 1)
+
+        if task_type == 'classification': 
+            cls._task_head_initializers[task_type][method] = initialization_function
+        else:
+            raise Exception(f"""Could not register task head initializer:
+                                {initialization_function.__name__},
+                                the name of the function should be of the form:
+                                (task_type)_(initialization_method).
+                                E.g.: classification_random(...)
+                            """
+            )
+
+        return initialization_function
+
+    @classmethod
+    def initialize_task_head(cls, task_type, method, init_kwargs): 
+        """
+        Method for initializing the weights of a task head. Reads in two strings, task_type and
+        method which jointly specify the task head and type of initialization method to use. 
+        Then calls on the corresponding function and forwards the **init_kwargs keyword arguments. 
+        
+        Args: 
+            * task_type (str): Type of task head (e.g. 'classification')
+            * method (str): Method to use for initializing the weights of the task head 
+                (e.g. 'random')
+            * init_kwargs (dict): Keyword arguments used by the initialization function 
+        Returns: 
+            * task_classifier_weights (dict): An arbitrary dictionary containing weights for 
+                the initialized task head. Depending on the task_type the weights returned 
+                might be different. 
+        """
+
+        try: 
+            initialization_function = self._task_head_initializers[task_type][method]
+        except KeyError:
+            raise Exception("Could not initialize task head; invalid task type or method")
+        
+        return initialization_function(**init_kwargs)
+        
---
+++ src/taskheads/classification.py
@@ -0,0 +1,102 @@
+__author__ = 'Richard Diehl Martinez'
+""" Defines a task head for classification tasks """
+
+import torch
+
+from .base import TaskHead
+
+class ClassificationHead(TaskHead):
+
+    loss_function = torch.nn.CrossEntropyLoss()
+
+    def __call__(self, input, labels, weights):
+        """ 
+        Runs a forward pass of the classification head 
+        """
+        logits = F.linear(last_hidden_state, **task_classifier_weights)
+        loss = self.loss_function(input=logits, target=labels)
+        
+        return (logits, loss)
+
+@TaskHead.register_initialization_method
+def classification_random(base_model_hidden_dim, n_classes, device, **kwargs):
+    """
+    Initializes classification task head using a random Xavier-He initialization method.
+
+    Args: 
+        * base_model_hidden_dim (int): The hidden dimensions of the outputs of the base_model 
+        * n_classes (int): Number of classes to classify over 
+        * device (str): Device type ('cuda' or 'cpu')
+    Returns: 
+        * task_classifier_weights (dict): {
+            * weight -> (torch.Tensor): classification weight matrix
+            * bias -> (torch.Tensor): classification bias vector
+            }
+    """
+    # Xavier normal weight implementation
+    std_weight = math.sqrt(2.0 / float(base_model_hidden_dim + n_classes))
+    std_bias = math.sqrt(2.0 / float(n_classes))
+
+    # weights need to be shape (out_features, in_features) to be compatible with linear layer
+    classifier_weight = torch.randn((n_classes, base_model_hidden_dim), device=device) \
+                            * std_weight
+    classifier_bias = torch.randn((n_classes), device=device) * std_bias
+
+    classifier_weight.requires_grad = True
+    classifier_bias.requires_grad = True
+
+    task_classifier_weights = {
+        "weight": classifier_weight,
+        "bias": classifier_bias
+    }
+
+    return task_classifier_weights
+
+
+# Registering new task head initialization method 
+@TaskHead.register_initialization_method
+def classification_protomaml(base_model_hidden_dim, n_classes, functional_model, params,
+                             data_batch, device, **kwargs):
+    """
+    Args: 
+        * base_model_hidden_dim (int): The hidden dimensions of the outputs of the base_model 
+        * n_classes (int): Number of classes to classify over 
+        * functional_model (higher.MonkeyPatched): The 'functionalized' version of the 
+            base model
+        * params ([torch.Tensor]): List of tensor weights storing the model weights.
+        * data_batch (dict): Batch of data for a forward pass through the model 
+            (see run_inner_loop for information on the data structure).
+        * device (str): Device type ('cuda' or 'cpu')
+    Returns: 
+        * task_classifier_weights (dict): {
+            * weight -> (torch.Tensor): classification weight matrix
+            * bias -> (torch.Tensor): classification bias vector
+            }
+    """
+
+    outputs = functional_model.forward(input_ids=data_batch['input_ids'],
+                                        attention_mask=data_batch['attention_mask'],
+                                        params=[p for p in params])
+
+    # outputs has form (batch_size, sequence_length, hidden_size);
+    batch_size = outputs.size(0)
+    last_hidden_state = outputs[torch.arange(batch_size), data_batch['input_target_idx']]
+
+    prototypes = torch.zeros((n_classes, base_model_hidden_dim), device=device)
+
+    for c in range(n_classes):
+        idx = torch.nonzero(data_batch['label_ids'] == c).squeeze()
+        if idx.nelement() != 0:
+            prototypes[c] = torch.mean(last_hidden_state[idx], dim=0)
+        else:
+            logger.warning("ProtoMaml weight initialization missing at least one class")
+
+    classifier_weight = 2 * prototypes
+    classifier_bias = -torch.norm(prototypes, dim=1)**2
+
+    task_classifier_weights = { 
+        "weight": classifier_weight,
+        "bias": classifier_bias
+    }
+
+    return task_classifier_weights
