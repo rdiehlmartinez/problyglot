10d5c209c2f93e15000c1fe20ced2a0528e3e38a
rdiehlmartinez <richarddiehl@gmail.com>
2022-05-03 17:27:29 +0100

    adding base implementation of maml

--- src/metalearners/base.py
+++ src/metalearners/base.py
@@ -239,3 +239,122 @@ class BaseLearner(torch.nn.Module, metaclass=abc.ABCMeta):
         """
         raise NotImplementedError()
 
+
+
+class MetaBaseLearner(BaseLearner):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def functionalize_model(self):
+        """ Helper function for converting base_model into a functionalized form"""
+        self.functional_model = higher.patch.make_functional(module=self.base_model)
+
+        # NOTE: these two lines are SUPER important (otherwise computation graph explodes)
+        self.functional_model.track_higher_grads = False
+        self.functional_model._fast_params = [[]]
+
+    # Overriding nn.Module functionality 
+    def state_dict(self):
+        """
+        Overriding method to remove placeholder parameters defined by functional model. Called 
+        implicitly when saving and loading checkpoints.
+        """
+        original_state_dict = super().state_dict()
+        updated_state_dict = OrderedDict()
+
+        for key, val in original_state_dict.items():
+            if "functional" in key:
+                continue
+            
+            updated_state_dict[key] = val
+        
+        return updated_state_dict
+
+    # Helper function for adapting the functionalized parameters based on some data_batch
+    def _adapt_params(self, data_batch, params, lm_head_weights, learning_rate, num_inner_steps,
+                            optimize_classifier=False, clone_params=True, evaluation_mode=False,
+                     ):
+        """ 
+        Adapted from: 
+        https://github.com/cnguyen10/few_shot_meta_learning
+
+        For a given batch of inputs and labels, we compute what the loss of the functional model 
+        would be if the weights of the model are set to params.  
+        
+        Note that this is the main reason that we have to convert the base model to a functional
+        version of the model that can take in its parameters as an argument
+        (as opposed to these parameters being stored in the model's state). 
+
+        The parameters are then updated using SGD with a given learning rate, and returned. 
+
+        Params:
+            * data_batch (dict): Batch of data for a forward pass through the model 
+                (see run_inner_loop for information on the data structure)
+            * params (iterable): Iterable of torch.nn.Paramater()s
+                (the params that we want to evaluate our model at)
+            * lm_head_weights (dict): Weights of the lm classification head
+            * learning_rate (float): The internal learning rate used to update params
+            * num_inner_steps (int): Number of inner steps to use for the adaptation process
+            * optimize_classifier (bool): Whether to train the final classification layer as 
+                part of the adaptation process 
+            * clone_params (bool): Whether to clone the params passed in (defaults to True)
+            * evaluation_mode (bool): Whether running this method during evaluation
+                (either in finetuning or inference) (defaults to False)
+
+        Returns: 
+            * adapted_params (iterable): Iterable of torch.nn.Paramater()s that represent the
+                updated the parameters after running SGD 
+        """                                        
+
+        if clone_params:
+            # copy the parameters but allow gradients to propagate back to original params
+            adapted_params = [torch.clone(p) for p in params]
+        else:
+            adapted_params = params
+
+        for _ in range(num_inner_steps):
+            
+            # Running forward pass through functioanl model and computing classificaiton loss
+            outputs = self.functional_model.forward(input_ids=data_batch['input_ids'],
+                                                    attention_mask=data_batch['attention_mask'],
+                                                    params=adapted_params)
+
+            _, loss = self._compute_task_loss(outputs, data_batch, lm_head_weights,
+                                              task_type='classification')
+                                                        
+            # Computing resulting gradients of the inner loop
+            grad_params = [p for p in adapted_params if p.requires_grad]
+
+            if self.use_first_order or evaluation_mode:
+                grads = torch.autograd.grad(
+                    outputs=loss,
+                    inputs=grad_params,
+                    retain_graph=True,
+                )
+            else:
+                grads = torch.autograd.grad(
+                    outputs=loss,
+                    inputs=grad_params,
+                    create_graph=True,
+                )
+
+            # updating params
+            grad_counter = 0
+            for i, p in enumerate(adapted_params):
+                if p.requires_grad:
+                    adapted_params[i] = adapted_params[i] - learning_rate * grads[grad_counter]
+                    grad_counter += 1
+
+            # optionally use SGD to update the weights of the final linear classification layer
+            # should only be done once we've already sampled phi weights
+            if optimize_classifier:
+                classifier_grads = torch.autograd.grad(
+                    outputs=loss,
+                    inputs=lm_head_weights.values(),
+                )
+                for idx, weight_name in enumerate(lm_head_weights.keys()):
+                    lm_head_weights[weight_name] = lm_head_weights[weight_name] - \
+                                                    self.classifier_lr * classifier_grads[idx]
+
+        return adapted_params
---
+++ src/metalearners/maml.py
@@ -0,0 +1,362 @@
+__author__ = 'Richard Diehl Martinez'
+"""
+Implements Model Agnostic Meta Learning: https://arxiv.org/abs/1703.03400
+"""
+
+import time
+import higher 
+import itertools
+
+from collections import OrderedDict
+from multiprocessing.queues import Empty as EmptyQueue
+
+import torch
+import torch.distributed as dist
+
+from .base import MetaBaseLearner
+from ..taskheads import TaskHead
+from ..utils import move_to_device
+
+class MAML(MetaBaseLearner):
+    def __init__(self, base_model,  optimizer_type='adam',
+                                    meta_lr=1e-2,
+                                    inner_lr=1e-2,
+                                    classifier_lr=1e-1,
+                                    num_learning_steps=5,
+                                    use_first_order=False,
+                                    *args,
+                                    **kwargs):
+        """
+        MAML implements a type of BaseLearner.
+
+        Reads in a base model and sets up all of the metalearning parameters. The core idea of 
+        MAML is to train a model using a two-loop approach - an outerloop that learns the learning 
+        process, and an inner loop in which the model is trained on a given meta-learning task.
+
+        Args: 
+            * base_model (implementation of BaseModel)
+            * optimizer_type (str) : The type of optimizer (e.g. 'adam') 
+            * meta_lr (float): Learning rate for the outer loop meta learning step
+            * The following keyword args define inner-loop hyper-parameters that can be 
+                meta-learned. The values passed in represent the initial values:
+                * inner_lr (float): inner-loop learning rate of the base_model 
+                * classifier_lr (float): inner-loop learning rate of the classifier head
+            * num_learning_steps (int): Number of gradients steps in the inner loop used 
+                to learn the meta-learning task
+            * use_first_order (bool): Whether a first order approximation of higher-order gradients
+                should be used (defaults to False)
+        """
+
+        super().__init__(base_model, *args, **kwargs)
+
+        # Initializing params of the functional model that will be meta-learned
+        self.model_params = torch.nn.ParameterList()
+        for param in base_model.parameters():
+            self.model_params.append(torch.nn.Parameter(data=param.data.to(self.base_device),
+                                                        requires_grad=param.requires_grad)
+                                )
+
+
+        self.inner_lr = torch.nn.Parameter(data=torch.tensor(float(inner_lr)).to(self.base_device))
+        self.classifier_lr = torch.nn.Parameter(data=torch.tensor(float(classifier_lr))\
+                                .to(self.base_device))
+
+
+        # loading in meta optimizer 
+        self.meta_lr = float(meta_lr)
+        if optimizer_type == 'adam': 
+            self.optimizer = torch.optim.Adam(params=self.meta_params_iter(), lr=self.meta_lr)
+        else:
+            logger.exception(f"Invalid optimizer type: {optimizer_type}")
+            raise Exception(f"Invalid optimizer type: {optimizer_type}")
+
+        # number of steps to perform in the inner loop
+        self.num_learning_steps = int(num_learning_steps)
+        
+        # set flag to indicate if first-order approximation should be used (à la Reptile)
+        if isinstance(use_first_order, str):
+            use_first_order = eval(use_first_order)
+        self.use_first_order = use_first_order
+
+    ###### Helper functions ######
+
+    def meta_params_iter(self):
+        """ Returns an iterator over all of the meta parameters"""
+        return itertools.chain(self.model_params, [self.inner_lr, self.classifier_lr],
+                               self.lm_head.values())
+
+    def parameters(self):
+        """ Overriding parent behavior to only return meta parameters """
+        return self.meta_params_iter()
+
+    def get_task_init_kwargs(self, task_init_method, n_labels, **kwargs):
+        """ 
+        Override base implementation of this method to replace the model with the functional 
+        model and also pass in the model params when the task head is initialized using  protomaml.
+
+        Args:
+            * task_init_method (str): Method for initializing the task head
+            * n_labels (int): Number of labels defined by the task (i.e. classes)
+        Returns:
+            * init_kwargs (dict): Keyword arguments used by the initialization function 
+        """
+
+        init_kwargs = super().get_task_init_kwargs(task_init_method, n_labels, **kwargs)
+        if 'protomaml' in task_init_method:
+            init_kwargs['model'] = self.functional_model
+            init_kwargs['params'] = self.model_params
+
+        return init_kwargs
+    
+
+    ###### Model training methods ######
+
+    ### Multi Processing Helper Method
+    def run_inner_loop_mp(self, rank, world_size, data_queue, loss_queue, step_optimizer, 
+                          num_tasks_per_iteration):
+        """
+        Entry point for running inner loop using multiple processes. Sets up DDP init process
+        group, wraps learner in DDP and calls forward/backward on the DDP-wrapped model.
+
+        Args: 
+            * rank (int): Rank of current GPU 
+            * world_size (int): Number of GPUs should be the same as utils.num_gpus
+            * data_queue (multiprocessing.Queue): Queue from which we read passed in data
+            * loss_queue (multiprocessing.Queue): Queue to which we write loss values
+            * step_optimizer (multiprocessing.Event): Event to signal workers to take an optimizer
+                step
+            * num_tasks_per_iteration (int): Number of tasks per iteration that the user specifies
+                in the experiment config file
+        """
+
+        device, ddp = self.setup_DDP(rank, world_size)
+
+        self.functionalize_model()
+
+        while True: 
+            # The main process sends signal to update optimizers
+
+            while True: 
+                # Waiting for the next batch of data 
+                # NOTE: If there is no data either 1) the dataloading pipeline is taking a while 
+                # or 2) the main process is waiting for all the workers to finish 
+                try:
+                    batch = data_queue.get(block=False)[0]
+                    break
+                except EmptyQueue: 
+                    pass
+
+                if step_optimizer.is_set():
+                    # make sure all workers have taken an optimizer step
+                    self.optimizer_step(set_zero_grad=True)
+                    dist.barrier()
+
+                    # once all workers have update params clear the flag to continue training
+                    step_optimizer.clear()
+
+                time.sleep(1) 
+
+            task_name, support_batch, query_batch = batch
+
+            task_loss = ddp(self, support_batch, query_batch, device)
+            task_loss = task_loss/num_tasks_per_iteration
+            task_loss.backward()
+
+            loss_queue.put([task_loss.detach().item()])
+
+    ### Main Inner Training Loop 
+    def run_inner_loop(self, support_batch, query_batch, device=None, *args, **kwargs): 
+        """
+        Implements the inner loop of the MAML process - clones the parameters of the model 
+        and trains those params using the support_batch for self.num_learning_steps number 
+        of steps using SGD.
+        
+        Args: 
+            * support_batch: A dictionary containing the following information for the support set
+                * input_ids (torch.tensor): Input tensors of shape (N*K, max_seq_len)
+                * input_target_idx (torch.tensor): Tensor indicating for each sample at what index
+                    we apply the final classification layer 
+                * label_ids (torch.tensor): Tensor of labels corresponding to masked out subword id
+                * attention_mask (torch.tensor): Tensor indicating which tokens in input_ids are
+                    not pad tokens
+            * query_batch: Same as support_batch, but for the data of the query set 
+            * device: Optional string to specify a device to override base_device
+
+        Returns: 
+            * loss (torch.Tensor): Loss value of the inner loop calculations
+        """
+        if device is None:
+            device = self.base_device
+
+        if not hasattr(self, "functional_model"):
+            # If not using multiprocessing training, the first iteration of run_inner_loop
+            # will have to functionalize the model 
+            self.functionalize_model()
+
+        self.functional_model.train()
+
+        # Moving data to appropriate device
+        support_batch = move_to_device(support_batch, device)
+        query_batch = move_to_device(query_batch, device)
+       
+        # cloning LM head in order to be locally adapted
+        adapted_lm_head = {key: torch.clone(param) for key, param in self.lm_head.items()}
+
+        # adapting params to the support set -> adapted params are phi
+        phi = self._adapt_params(support_batch, 
+                                 params=self.model_params, 
+                                 lm_head_weights=adapted_lm_head,
+                                 learning_rate=self.inner_lr,
+                                 num_inner_steps=self.num_learning_steps,
+                                 clone_params=True,
+                                 optimize_classifier=True)
+
+        # evaluating on the query batch using the adapted params phi  
+        self.functional_model.eval()
+
+        outputs = self.functional_model.forward(input_ids=query_batch['input_ids'],
+                                                attention_mask=query_batch['attention_mask'],
+                                                params=phi)
+
+        self.functional_model.train()
+
+        _, loss = self._compute_task_loss(outputs, query_batch, adapted_lm_head, 
+                                             task_type='classification')
+        return loss
+
+
+    ###### Model evaluation methods ######
+
+    def run_finetuning(self, task_type, finetune_dataloader, n_labels, task_head_init_method,
+                       max_finetuning_batch_steps=-1, **kwargs): 
+        """
+        Creates a copy of the trained model parameters and continues to finetune these 
+        parameters on a given dataset. 
+        
+        Args: 
+            * task_type (str): Type of task (e.g. 'classification')
+            * finetune_dataloader (torch.data.Dataloader): The dataset for finetuning the model on
+                is passed in as a dataloader (i.e. NLUDataloader)
+            * n_labels (int): The number of labels in the given finetuning task
+            * task_head_init_method (str): Method for initializing task head 
+            * max_finetuning_batch_steps (int): Optional maximum number of batch steps to take 
+                for model finetuning 
+
+        Returns:
+            * inference_params dict containing: 
+                * finetuned_params ([nn.Parameter]): List of the finetuned model's parameters
+                * task_head_weights (dict): Weights of task head (classifier head)
+        """
+
+        if not hasattr(self, "functional_model"):
+            # NOTE: Edge case for if the model is only being evaluated without having 
+            # been trained
+            self.functionalize_model()
+
+        self.functional_model.train()
+
+        ### Initializing the task head used for the downstream NLU task
+        task_init_data_batch = move_to_device(next(iter(finetune_dataloader)), self.base_device)
+        init_kwargs = self.get_task_init_kwargs(task_head_init_method, n_labels=n_labels,
+                                                data_batch=task_init_data_batch)
+        task_head_weights = TaskHead.initialize_task_head(task_type=task_type,
+                                                          method=task_head_init_method,
+                                                          init_kwargs=init_kwargs)
+
+        # detaching parameters from original computation graph to create new leaf variables
+        finetuned_model_params = []
+        for p in self.model_params:
+            detached_p = p.clone().detach()
+            detached_p.requires_grad = p.requires_grad
+            finetuned_model_params.append(detached_p)
+
+        finetuned_task_head_weights = {}
+        for k, p in task_head_weights.items():
+            detached_p = p.detach()
+            detached_p.requires_grad = True
+            finetuned_task_head_weights[k] = detached_p
+
+        finetune_params = itertools.chain(finetuned_model_params,
+                                          finetuned_task_head_weights.values())
+        finetune_optimizer = torch.optim.Adam(params=finetune_params)
+
+        for batch_idx, data_batch in enumerate(finetune_dataloader):
+            data_batch = move_to_device(data_batch, self.base_device)
+            finetune_optimizer.zero_grad()
+
+            # run SGD on the finetuned parameters
+
+            outputs = self.functional_model.forward(input_ids=data_batch['input_ids'],
+                                                    attention_mask=data_batch['attention_mask'],
+                                                    params=finetuned_model_params)
+
+            _, loss = self._compute_task_loss(outputs, data_batch, finetuned_task_head_weights, 
+                                              task_type=task_type)
+
+            loss.backward()
+            finetune_optimizer.step()
+
+            if max_finetuning_batch_steps > 0 and (batch_idx + 1) >= max_finetuning_batch_steps:
+                break
+
+        inference_params = {
+            "finetuned_params": finetuned_model_params, 
+            "task_head_weights": finetuned_task_head_weights
+        }
+
+        return inference_params
+
+
+    def run_inference(self, task_type, inference_dataloader, finetuned_params, task_head_weights,
+                      **kwargs):
+        """ 
+        This method is to be called after run_finetuning. 
+        
+        As the name suggests, this method runs inference on an NLU dataset for some task.
+
+        Args: 
+            * task_type (str): Type of task (e.g. 'classification')
+            * inference_dataloader (torch.data.Dataloader): The dataset for inference is passed
+                in as a dataloader (in most cases this will be an NLUDataloader)
+            * finetuned_params ([nn.Parameter]): List of the finetuned model's parameters
+            * task_head_weights (dict): Weights of task head (classifier head)
+
+        Returns: 
+            * predictions ([int]): A dictionary storing the model's predictions for each 
+                datapoint passed in from the inference_dataloader as an int. 
+            * loss (int): The value of the classification loss on the inference dataset.
+        """
+        if not hasattr(self, "functional_model"):
+            # If the model is only being evaluated (and not being finetuned) it might not have
+            # a functionalized version
+            self.functionalize_model()
+        
+        predictions = []
+        total_loss = 0.0
+        total_samples = 0
+
+        # Running final inference script over the evaluation data
+        with torch.no_grad():
+            self.functional_model.eval()
+
+            for data_batch in inference_dataloader: 
+                data_batch = move_to_device(data_batch, self.base_device)
+
+                outputs = self.functional_model.forward(input_ids=data_batch['input_ids'],
+                                                        attention_mask=data_batch['attention_mask'],
+                                                        params=finetuned_params)
+
+                logits, loss = self._compute_task_loss(outputs, data_batch, task_head_weights,
+                                                       task_type=task_type)
+
+                predictions.extend(torch.argmax(logits, dim=-1).tolist())
+
+                batch_size = logits.size(0)
+                total_loss += loss.item() * batch_size # loss is averaged across batch
+                total_samples += batch_size 
+
+            total_loss /= total_samples
+
+            self.functional_model.train()
+
+        return (predictions, total_loss)
